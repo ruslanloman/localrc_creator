b6f03a66661a1fd34a839bbcdd39c0eef42e083b
rusichenko <rusichenko@mirantis.com>
2014-01-22 16:26:53 +0200

    dddd

--- devstack-vm-gate.sh
+++ devstack-vm-gate.sh
@@ -19,7 +19,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #put to job export 
-export LOCALRC_BRANCH=${BRANCH:-"master"}
+export LOCALRC_BRANCH=${BRANCH}
 
 export DEVSTACK_GATE_TEMPEST=${DEVSTACK_GATE_TEMPEST:-0}
 
